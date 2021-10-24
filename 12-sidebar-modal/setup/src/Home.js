d04d3d6bcf22f245c9df988200c9f31e60c45aca
parvathy <parvathyprasad505@gmail.com>
2021-10-24 15:10:05 +0530

    first commit

---
+++ 12-sidebar-modal/setup/src/Home.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { AppContext,useGlobalContext } from './context'
+const Home = () => {
+  const {openSidebar,openModel}=useGlobalContext()
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars/>
+      </button>
+      <button className="btn" onClick={openModel}>show modal</button>
+    </main>
+  )
+}
+
+export default Home
